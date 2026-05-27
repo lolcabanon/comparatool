ee3cce7682b45e35c2ce554db1d8f835350e56d7
lolcabanon <julesses@gmail.com>
2026-05-27 16:40:27 -0400

    fmt

--- src/lib/_frCA.js
+++ src/lib/_frCA.js
@@ -8,107 +8,107 @@ import { match } from '../../node_modules/date-fns/locale/fr/_lib/match.mjs';
 import { formatLong } from '../../node_modules/date-fns/locale/fr-CA/_lib/formatLong.mjs';
 
 const formatDistanceLocale = {
-  lessThanXSeconds: {
-    one: '- d’une seconde',
-    other: '- de {{count}} secondes'
-  },
+	lessThanXSeconds: {
+		one: '- d’une seconde',
+		other: '- de {{count}} secondes'
+	},
 
-  xSeconds: {
-    one: '1 seconde',
-    other: '{{count}} secondes'
-  },
+	xSeconds: {
+		one: '1 seconde',
+		other: '{{count}} secondes'
+	},
 
-  halfAMinute: '30 secondes',
+	halfAMinute: '30 secondes',
 
-  lessThanXMinutes: {
-    one: '- d’une minute',
-    other: '- de {{count}} minutes'
-  },
+	lessThanXMinutes: {
+		one: '- d’une minute',
+		other: '- de {{count}} minutes'
+	},
 
-  xMinutes: {
-    one: '1 minute',
-    other: '{{count}} minutes'
-  },
+	xMinutes: {
+		one: '1 minute',
+		other: '{{count}} minutes'
+	},
 
-  aboutXHours: {
-    one: '~ 1 heure',
-    other: '~ {{count}} heures'
-  },
+	aboutXHours: {
+		one: '~ 1 heure',
+		other: '~ {{count}} heures'
+	},
 
-  xHours: {
-    one: '1 heure',
-    other: '{{count}} heures'
-  },
+	xHours: {
+		one: '1 heure',
+		other: '{{count}} heures'
+	},
 
-  xDays: {
-    one: '1 jour',
-    other: '{{count}} jours'
-  },
+	xDays: {
+		one: '1 jour',
+		other: '{{count}} jours'
+	},
 
-  aboutXWeeks: {
-    one: '~ 1 semaine',
-    other: '~ {{count}} semaines'
-  },
+	aboutXWeeks: {
+		one: '~ 1 semaine',
+		other: '~ {{count}} semaines'
+	},
 
-  xWeeks: {
-    one: '1 semaine',
-    other: '{{count}} semaines'
-  },
+	xWeeks: {
+		one: '1 semaine',
+		other: '{{count}} semaines'
+	},
 
-  aboutXMonths: {
-    one: '~ 1 mois',
-    other: '~ {{count}} mois'
-  },
+	aboutXMonths: {
+		one: '~ 1 mois',
+		other: '~ {{count}} mois'
+	},
 
-  xMonths: {
-    one: '1 mois',
-    other: '{{count}} mois'
-  },
+	xMonths: {
+		one: '1 mois',
+		other: '{{count}} mois'
+	},
 
-  aboutXYears: {
-    one: '~ 1 an',
-    other: '~ {{count}} ans'
-  },
+	aboutXYears: {
+		one: '~ 1 an',
+		other: '~ {{count}} ans'
+	},
 
-  xYears: {
-    one: '1 an',
-    other: '{{count}} ans'
-  },
+	xYears: {
+		one: '1 an',
+		other: '{{count}} ans'
+	},
 
-  overXYears: {
-    one: '+ d’un an',
-    other: '+ de {{count}} ans'
-  },
+	overXYears: {
+		one: '+ d’un an',
+		other: '+ de {{count}} ans'
+	},
 
-  almostXYears: {
-    one: '\u2272 un an',
-    other: '\u2272 {{count}} ans'
-  }
+	almostXYears: {
+		one: '\u2272 un an',
+		other: '\u2272 {{count}} ans'
+	}
 };
 
 /**
  * @type {import('date-fns').FormatDistanceFn}
  */
 export const formatDistance = (token, count, options) => {
-  let result;
-  const form = formatDistanceLocale[token];
-  if (typeof form === 'string') {
-    result = form;
-  } else if (count === 1) {
-    result = form.one;
-  } else {
-    result = form.other.replace('{{count}}', String(count));
-  }
+	let result;
+	const form = formatDistanceLocale[token];
+	if (typeof form === 'string') {
+		result = form;
+	} else if (count === 1) {
+		result = form.one;
+	} else {
+		result = form.other.replace('{{count}}', String(count));
+	}
 
-  if (options?.addSuffix) {
-    if (options.comparison && options.comparison > 0) {
-      return 'dans ' + result;
-    } else {
-      return 'il y a ' + result;
-    }
-  }
+	if (options?.addSuffix) {
+		if (options.comparison && options.comparison > 0) {
+			return 'dans ' + result;
+		} else {
+			return 'il y a ' + result;
+		}
+	}
 
-  return result;
+	return result;
 };
 
 /**
@@ -121,22 +121,22 @@ export const formatDistance = (token, count, options) => {
  * @author Gabriele Petrioli [@gpetrioli](https://github.com/gpetrioli)
  */
 export const frCA = {
-  code: 'fr-CA',
-  formatDistance: formatDistance,
-  formatLong: formatLong,
-  formatRelative: formatRelative,
-  localize: localize,
-  match: match,
+	code: 'fr-CA',
+	formatDistance: formatDistance,
+	formatLong: formatLong,
+	formatRelative: formatRelative,
+	localize: localize,
+	match: match,
 
-  /**
-   * Unique for fr-CA
-   *
-   * @type {import('date-fns').LocaleOptions}
-   */
-  options: {
-    weekStartsOn: 0 /* Sunday */,
-    firstWeekContainsDate: 1
-  }
+	/**
+	 * Unique for fr-CA
+	 *
+	 * @type {import('date-fns').LocaleOptions}
+	 */
+	options: {
+		weekStartsOn: 0 /* Sunday */,
+		firstWeekContainsDate: 1
+	}
 };
 
 // Fallback for modularized imports:
--- src/routes/[type]/+page.server.js
+++ src/routes/[type]/+page.server.js
@@ -11,89 +11,86 @@ import { getNPMpackage } from '$lib/server/getNPM';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
-  const tools = await prisma.tool.findMany({
-    where: {
-      typeStr: params.type
-    },
-    include: {
-      type: true
-    }
-  });
+	const tools = await prisma.tool.findMany({
+		where: {
+			typeStr: params.type
+		},
+		include: {
+			type: true
+		}
+	});
 
-  // const pkgs = [] ?? (await getNPMpackages(tools));
+	// const pkgs = [] ?? (await getNPMpackages(tools));
 
-  return { tools };
+	return { tools };
 }
 
 /** @type {import('./$types').Actions} */
 export const actions = {
-  default: async ({ request, fetch, params }) => {
-    const data = await request.formData();
+	default: async ({ request, fetch, params }) => {
+		const data = await request.formData();
 
-    try {
-      const repoURL = repoURLSchema.parse(data.get('repo_url'));
+		try {
+			const repoURL = repoURLSchema.parse(data.get('repo_url'));
 
-      console.log('OK', repoURL);
+			console.log('OK', repoURL);
 
-      const repoRoute = new URL(repoURL);
+			const repoRoute = new URL(repoURL);
 
-      const repoInfos = await fetch(
-        `https://api.github.com/repos${repoRoute.pathname}`,
-        ghAuth()
-      );
+			const repoInfos = await fetch(`https://api.github.com/repos${repoRoute.pathname}`, ghAuth());
 
-      const repoJSON = repoJSONSchema.parse(await repoInfos.json());
+			const repoJSON = repoJSONSchema.parse(await repoInfos.json());
 
-      const [, pkgName] = await getNPMpackage({
-        name: repoJSON.name,
-        repo: repoURL
-      });
+			const [, pkgName] = await getNPMpackage({
+				name: repoJSON.name,
+				repo: repoURL
+			});
 
-      const tool = await prisma.tool.create({
-        data: {
-          typeStr: params.type,
+			const tool = await prisma.tool.create({
+				data: {
+					typeStr: params.type,
 
-          name: repoJSON.name,
-          repo: repoURL,
-          homepage: repoJSON.homepage,
-          pkgName,
+					name: repoJSON.name,
+					repo: repoURL,
+					homepage: repoJSON.homepage,
+					pkgName,
 
-          license: repoJSON.license?.spdx_id ?? 'Inconnue',
+					license: repoJSON.license?.spdx_id ?? 'Inconnue',
 
-          created_at: repoJSON.created_at,
-          updated_at: repoJSON.updated_at,
-          pushed_at: repoJSON.pushed_at,
+					created_at: repoJSON.created_at,
+					updated_at: repoJSON.updated_at,
+					pushed_at: repoJSON.pushed_at,
 
-          stargazers_count: repoJSON.stargazers_count,
-          open_issues_count: repoJSON.open_issues_count
-        },
-        include: {
-          type: true
-        }
-      });
+					stargazers_count: repoJSON.stargazers_count,
+					open_issues_count: repoJSON.open_issues_count
+				},
+				include: {
+					type: true
+				}
+			});
 
-      return { tool };
-    } catch (err) {
-      console.error(err);
-      // error(400);
+			return { tool };
+		} catch (err) {
+			console.error(err);
+			// error(400);
 
-      let message = 'Erreur inconnue';
+			let message = 'Erreur inconnue';
 
-      if (err instanceof ZodError) {
-        message = 'Erreur de validation des champs.';
-      } else if (err instanceof PrismaClientKnownRequestError) {
-        switch (err.code) {
-          case 'P2002':
-            message = 'Cet outil est déjà répertorié!';
-            break;
-          default:
-            message = 'Erreur DB inconnue';
-        }
-      }
+			if (err instanceof ZodError) {
+				message = 'Erreur de validation des champs.';
+			} else if (err instanceof PrismaClientKnownRequestError) {
+				switch (err.code) {
+					case 'P2002':
+						message = 'Cet outil est déjà répertorié!';
+						break;
+					default:
+						message = 'Erreur DB inconnue';
+				}
+			}
 
-      return fail(400, {
-        message
-      });
-    }
-  }
+			return fail(400, {
+				message
+			});
+		}
+	}
 };

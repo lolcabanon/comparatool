744ae5d8fa51c25024ff5f6d886cf561054dfd4c
lolcabanon <julesses@gmail.com>
2026-05-27 16:41:47 -0400

    prisma cursed black magic

---
+++ src/lib/server/prisma.ts
@@ -0,0 +1,16 @@
+// import { PrismaClient } from '@prisma/client';
+
+// export const prisma = new PrismaClient();
+
+import type { PrismaClient as ImportedPrismaClient } from '@prisma/client';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url ?? __filename);
+
+const { PrismaClient: RequiredPrismaClient } = require('@prisma/client');
+
+const _PrismaClient: typeof ImportedPrismaClient = RequiredPrismaClient;
+
+export class PrismaClient extends _PrismaClient {}
+
+export const prisma = new PrismaClient();
